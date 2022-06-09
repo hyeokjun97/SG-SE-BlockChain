a0ad5ddfa13f7a184b7f2742a2cf2275062f61af
jeremy-yoon <calamansi@calamansiui-MacBookAir.local>
2022-06-09 23:17:32 +0900

    추가: send 관련한 error 처리

--- src/component/SendCoin.js
+++ src/component/SendCoin.js
@@ -11,8 +11,10 @@ import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select, { SelectChangeEvent } from "@mui/material/Select";
 import SendCoinAPI from "../dao/SendCoinAPI.js";
+import QueryCoinAPI from "../dao/QueryCoinAPI.js";
 import * as tokenData from "../dao/data/token";
 import * as userData from "../dao/data/user";
+import { web3 } from "../dao/helper/web3Helper";
 
 function preventDefault(event) {
   event.preventDefault();
@@ -20,9 +22,10 @@ function preventDefault(event) {
 
 export default function SendCoin({ onSend }) {
   const [token, setToken] = React.useState("syc");
-  const [address, setAddress] = React.useState(null);
-  const [amount, setAmount] = React.useState(null);
+  const [address, setAddress] = React.useState("");
+  const [amount, setAmount] = React.useState("");
   const [tokenList, setTokenList] = React.useState([]);
+  const [curUser, setCurUser] = React.useState();
   let node1Address = "";
 
   const getTokenList = () => {
@@ -30,19 +33,50 @@ export default function SendCoin({ onSend }) {
     const tokens = tokenData.getByUserAddr(user.data.address);
     console.log("SendCoint JS Token: ", tokens);
     setTokenList(tokens.data);
+    if (tokens.data.length > 0) {
+      setToken(tokens.data[0]);
+    }
+    setCurUser(user.data);
   };
 
   const sendCoin = async () => {
     //TODO 실제 전송
     // alert('little sendcoin');
-    await SendCoinAPI.instance.sendCoin(
+
+    // console.log(web3.utils.isAddress("asdf"));
+    if (address.length == 0) {
+      return alert("주소를 적어주세요.");
+    }
+    if (amount.length == 0) {
+      return alert("수량을 적어주세요");
+    }
+
+    const balance = await QueryCoinAPI.instance.getBalance(
+      curUser.address,
+      token.cont_addr
+    );
+
+    if (balance < parseInt(amount)) {
+      return alert("가지고 있는 토큰보다 많습니다.");
+    }
+
+    const result = await SendCoinAPI.instance.sendCoin(
       token.cont_addr,
       address,
-      parseInt(amount) //만약 소수점까지 보내야할때 이부분을 10^-10자리수 까지 보내야함. 나중에 수정 필요
-      //수정 완료. 
+      parseFloat(amount) //만약 소수점까지 보내야할때 이부분을 10^-10자리수 까지 보내야함. 나중에 수정 필요
+      //수정 완료.
     );
-    console.log("DEBUG SendCoin:", amount);
-    onSend(token, address, amount);
+    // console.log("DEBUG SendCoin:", amount);
+    if (result.status == 200) {
+      alert(result.data);
+      window.location.reload();
+    }
+    if (result.status == 404 || result.status == 400 || result.status == 500) {
+      alert(result.data);
+    }
+    setAddress("");
+    setAmount("");
+    // onSend(token, address, amount);
   };
 
   React.useEffect(() => {
@@ -59,6 +93,7 @@ export default function SendCoin({ onSend }) {
           labelId="demo-simple-select-label"
           id="token"
           label="Token"
+          value={token}
           onChange={(event) => {
             setToken(event.target.value);
           }}
@@ -73,11 +108,12 @@ export default function SendCoin({ onSend }) {
         margin="normal"
         required
         fullWidth
-        name="target_address"
+        // name="target_address"
         label="주소 (Address)"
         type="text"
-        id="target_address"
-        autoComplete="target_address"
+        // id="target_address"
+        // autoComplete="target_address"
+        value={address}
         onChange={(e) => {
           setAddress(e.target.value);
         }}
@@ -91,7 +127,8 @@ export default function SendCoin({ onSend }) {
         label={`수량 (Amount/${token.token_symbol})`}
         type="number"
         id="amount"
-        autoComplete="amount"
+        // autoComplete="amount"
+        value={amount}
         onChange={(e) => {
           setAmount(e.target.value);
         }}
--- src/dao/controller/send.js
+++ src/dao/controller/send.js
@@ -13,69 +13,146 @@ export const send = async (web3, cont_addr, target_addr, value) => {
   //현재 코드에서는 정수로 인자를 받는다. 이부분 수정시 SendCoinAPI / "SendCointjs" 부분을 수정
   //수정 완료.
   //여기서 고치자
+
+  //FIND THE CURRENT USER
+
   let convertedValue = value * Math.pow(10, 10);
-  //console.log("Converted ", convertedValue );
+  console.log("Converted ", convertedValue);
+
+  const me = userData.getCurrentUser();
+  const isAddr = web3.utils.isAddress(target_addr);
+  console.log(isAddr);
+  if (!web3.utils.isAddress(target_addr)) {
+    return { data: "address가 잘못되었습니다", status: 404 };
+  }
+
   var contract = new web3.eth.Contract(ABI, cont_addr);
   var set_contract = contract.methods.transfer(target_addr, convertedValue);
 
   var set_contract_byte = set_contract.encodeABI();
 
-  //FIND THE CURRENT USER
-  const me = userData.getCurrentUser();
-
-  if (!web3.utils.isAddress(target_addr)) {
-    return { data: "address가 잘못되었습니다", status: 404 };
-  }
   const balance = await contract.methods.balanceOf(target_addr);
+  console.log(balance);
   if (balance < convertedValue) {
     return { data: "자산이 부족합니다", status: 400 };
   }
 
-  web3.eth.getTransactionCount(me.data.address, "pending", (err, nonce) => {
-    //console.log("MY Address ", me.data.address);
-    if (err) {
-      return;
-    }
-    const allTransaction = transactionData.get();
-    console.log("Nounce: ", nonce);
-    var Raw_Tx = {
-      nonce: web3.utils.toHex(nonce ),
-      gasPrice: web3.utils.toHex(1000),
-      gasLimit: web3.utils.toHex(3000000),
-      data: set_contract_byte,
-      from: me.data.address,
-      to: cont_addr,
-    };
+  const nonce = await web3.eth.getTransactionCount(me.data.address, "pending");
 
-    // priv_key 가져와야
-    //const me = userData.getCurrentUser();
-    const userPrivKey = Buffer.from(me.data.private_key, "hex");
-    console.log("My Address ", me.data.address);
-    var Signature = new Buffer.from(userPrivKey, "hex");
+  const allTransaction = transactionData.get();
+  console.log("Nounce: ", nonce);
+  var Raw_Tx = {
+    nonce: web3.utils.toHex(nonce),
+    gasPrice: web3.utils.toHex(1000),
+    gasLimit: web3.utils.toHex(3000000),
+    data: set_contract_byte,
+    from: me.data.address,
+    to: cont_addr,
+  };
 
-    var Make_Tx = new ethTx(Raw_Tx);
-    Make_Tx.sign(Signature);
+  // priv_key 가져와야
+  //const me = userData.getCurrentUser();
+  const userPrivKey = Buffer.from(me.data.private_key, "hex");
+  console.log("My Address ", me.data.address);
+  var Signature = new Buffer.from(userPrivKey, "hex");
 
-    //console.log("DEBUG Make_Tx Done");
+  var Make_Tx = new ethTx(Raw_Tx);
+  Make_Tx.sign(Signature);
 
-    var Serialized_Tx = Make_Tx.serialize();
-    var Raw_Tx_Hex = "0x" + Serialized_Tx.toString("hex");
+  //console.log("DEBUG Make_Tx Done");
 
-    //console.log("DEBUG Raw_Tx_Hex Done");
+  var Serialized_Tx = Make_Tx.serialize();
+  var Raw_Tx_Hex = "0x" + Serialized_Tx.toString("hex");
 
-    web3.eth.sendSignedTransaction(Raw_Tx_Hex).on("receipt", (receipt) => {
-      console.log("receipt : ", receipt, cont_addr);
-      transactionData.add({
-        hash: receipt.transactionHash,
-        cont_addr: cont_addr,
-        receiver: target_addr,
-        sent_amount: convertedValue,
-        date: new Date(),
-        sender: me.data.address,
-      });
+  //console.log("DEBUG Raw_Tx_Hex Done");
+
+  try {
+    const receipt = await web3.eth.sendSignedTransaction(Raw_Tx_Hex);
+    console.log("receipt : ", receipt, cont_addr);
+    transactionData.add({
+      hash: receipt.transactionHash,
+      cont_addr: cont_addr,
+      receiver: target_addr,
+      sent_amount: convertedValue,
+      date: new Date(),
+      sender: me.data.address,
     });
-    //console.log("Transaction DATA ", transactionData.getByContractAddr(cont_addr));
-    //console.log("EXAMPLE USE OF function");
-    web3.eth.getTransaction("0x96efa7a92bee62d0f553f2510f1e5d87c7b206656a98bef8efce5da72665a1e9").then(console.log);
-  });
+    return { data: "전송이 완료되었습니다", status: 200 };
+    // alert("전송이 완료되었습니다");
+  } catch (e) {
+    return { data: "네트워크에 에러가 발생하였습니다.", status: 500 };
+  }
+
+  // web3.eth.sendSignedTransaction(Raw_Tx_Hex).on("receipt", (receipt) => {
+  //   console.log("receipt : ", receipt, cont_addr);
+  //   transactionData.add({
+  //     hash: receipt.transactionHash,
+  //     cont_addr: cont_addr,
+  //     receiver: target_addr,
+  //     sent_amount: convertedValue,
+  //     date: new Date(),
+  //     sender: me.data.address,
+  //   });
+  //   alert("전송이 완료되었습니다");
+  // });
+  //console.log("Transaction DATA ", transactionData.getByContractAddr(cont_addr));
+  //console.log("EXAMPLE USE OF function");
+  // web3.eth
+  //   .getTransaction(
+  //     "0x96efa7a92bee62d0f553f2510f1e5d87c7b206656a98bef8efce5da72665a1e9"
+  //   )
+  //   .then(console.log);
+
+  // web3.eth.getTransactionCount(me.data.address, "pending", (err, nonce) => {
+  //   //console.log("MY Address ", me.data.address);
+  //   if (err) {
+  //     return;
+  //   }
+  //   const allTransaction = transactionData.get();
+  //   console.log("Nounce: ", nonce);
+  //   var Raw_Tx = {
+  //     nonce: web3.utils.toHex(nonce),
+  //     gasPrice: web3.utils.toHex(1000),
+  //     gasLimit: web3.utils.toHex(3000000),
+  //     data: set_contract_byte,
+  //     from: me.data.address,
+  //     to: cont_addr,
+  //   };
+
+  //   // priv_key 가져와야
+  //   //const me = userData.getCurrentUser();
+  //   const userPrivKey = Buffer.from(me.data.private_key, "hex");
+  //   console.log("My Address ", me.data.address);
+  //   var Signature = new Buffer.from(userPrivKey, "hex");
+
+  //   var Make_Tx = new ethTx(Raw_Tx);
+  //   Make_Tx.sign(Signature);
+
+  //   //console.log("DEBUG Make_Tx Done");
+
+  //   var Serialized_Tx = Make_Tx.serialize();
+  //   var Raw_Tx_Hex = "0x" + Serialized_Tx.toString("hex");
+
+  //   //console.log("DEBUG Raw_Tx_Hex Done");
+
+  //   web3.eth.sendSignedTransaction(Raw_Tx_Hex).on("receipt", (receipt) => {
+  //     console.log("receipt : ", receipt, cont_addr);
+  //     transactionData.add({
+  //       hash: receipt.transactionHash,
+  //       cont_addr: cont_addr,
+  //       receiver: target_addr,
+  //       sent_amount: convertedValue,
+  //       date: new Date(),
+  //       sender: me.data.address,
+  //     });
+  //     alert("전송이 완료되었습니다");
+  //   });
+  //   //console.log("Transaction DATA ", transactionData.getByContractAddr(cont_addr));
+  //   //console.log("EXAMPLE USE OF function");
+  //   web3.eth
+  //     .getTransaction(
+  //       "0x96efa7a92bee62d0f553f2510f1e5d87c7b206656a98bef8efce5da72665a1e9"
+  //     )
+  //     .then(console.log);
+  // });
 };
